a3d31a6f89232a5d7ed30eab87c716a47b06dbcf
c-Stats <77307483+c-Stats@users.noreply.github.com>
2021-09-23 16:59:10 -0400

    Add files via upload

---
+++ warzone whitelisting checker plots.R
@@ -0,0 +1,125 @@
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+library(reshape2)
+
+
+path <- "D:/CoD_data/"
+names <- c("FrankFredj%231458", "Huskerrs", "SwaggXBL")
+
+frames <- lapply(as.list(names), function(x){fread(paste(path, x, ".csv", sep = ""))})
+names(frames) <- names
+
+#format dates
+i <- 1
+for(f in frames){
+
+	if("V1" %in% names(f)){
+
+		f[, V1 := NULL]
+
+	}
+
+	#remove hackers
+
+	index <- f[grepl("PM", Time), which = TRUE]
+	
+	f[index, Hour := as.numeric(str_split(f$Time[index], ":", n = 2, simplify = TRUE)[, 1]) + 12]
+	f[-index, Hour := as.numeric(str_split(f$Time[-index], ":", n = 2, simplify = TRUE)[, 1])]
+
+	f[, Minute := as.numeric(str_split(f$Time, ":", n = 3, simplify = TRUE)[, 2])]
+
+	f[Minute > 30, Hour := Hour + 1]
+	f[Hour == 25, Hour := 1]
+
+	f[, Time_Group := paste("TimeGroup", floor((Hour - 1) / 3), sep = "")]
+	f[, Player := names[i]]
+
+	f[, Date := NULL] %>%
+		.[, Time := NULL] %>%
+		.[, Hour := NULL] %>%
+		.[, Minute := NULL] 
+
+	i <- i + 1
+
+}
+
+frames <- dplyr::bind_rows(frames)
+frames <- frames[KDR <= 7.5] 
+
+frames <- frames[(grepl("BR", Mode) | grepl("Resurgence", Mode)) & !(grepl("Buyback", Mode))]
+
+frames[grepl("BR", Mode), Mode := "BR"] %>%
+		.[grepl("Resurgence", Mode), Mode := "Resurgence"]
+
+frames[, (names(frames)[sapply(frames, is.character)]) := lapply(.SD, as.factor), .SDcols = names(frames)[sapply(frames, is.character)]]
+
+
+#fixed effects model
+fixed_effect_model <- lm(KDR ~., frames)
+
+anova_table <- anova(fixed_effect_model)
+individual_p_vals <- summary(fixed_effect_model)$coefficients
+
+#Remove Resurgence since streamers don't play it that much
+#Also, it seems like SBMM works differently in BR vs Resurgence
+
+frames <- frames[Mode == "BR"] %>%
+					.[, Mode := NULL]
+
+
+#Re-fit model after deleting Mode
+fixed_effect_model <- lm(KDR ~., frames)
+
+anova_table <- anova(fixed_effect_model)
+individual_p_vals <- summary(fixed_effect_model)$coefficients
+
+
+
+#BR plot
+#Remove the effect due to game mode, and game time
+coefficients <- coef(fixed_effect_model)
+coefficients <- coefficients[which(grepl("Player", names(coefficients)))]
+coefficients_names <- names(coefficients)
+
+adjusted_kdr <- frames[, c("KDR", "Player"), with = FALSE] %>%
+						.[, Adjustment := 0]
+
+for(i in 1:length(coefficients)){
+
+	j <- str_remove(names(coefficients[i]), "Player")
+	adjusted_kdr[Player == j, Adjustment := coefficients[i]] 
+
+}
+
+adjusted_kdr[, KDR := fixed_effect_model$residuals + 
+						coef(fixed_effect_model)[1] +
+						Adjustment]
+
+adjusted_kdr[, Adjustment := NULL]
+
+
+
+#Plot
+plot_frame <- melt(adjusted_kdr, id = "Player")
+ggplot(plot_frame, aes(x = value, fill = Player)) + geom_density(alpha = 0.5)
+
+sample_desc_stats <- lapply(as.list(c(length, mean, median, sd)),
+							function(x){adjusted_kdr[, lapply(.SD, x), by = "Player", .SDcols = "KDR"]})
+
+names(sample_desc_stats) <- c("length", "mean", "median", "sd")
+
+
+#T-test
+t.test(adjusted_kdr[Player == "FrankFredj%231458"]$KDR,
+			adjusted_kdr[Player == "Huskerrs"]$KDR)
+
+t.test(adjusted_kdr[Player == "FrankFredj%231458"]$KDR,
+			adjusted_kdr[Player == "SwaggXBL"]$KDR)		
+
+
+
+
+
---
+++ warzone whitelisting checker.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+import multiprocessing
+import concurrent.futures
+
+from tqdm import tqdm
+
+import requests
+import urllib
+
+import pandas as pd
+import numpy as np
+
+webdriver_path = "D:/MLB/MLB_Modeling/chromedriver"
+
+
+urls = ["https://cod.tracker.gg/warzone/profile/battlenet/FrankFredj%231458/matches",
+		"https://cod.tracker.gg/warzone/profile/atvi/Huskerrs/overview",
+		"https://cod.tracker.gg/warzone/profile/xbl/SwaggXBL/overview"]
+
+driver = webdriver.Chrome(webdriver_path)
+
+#Anti-robot captcha
+#Manually log in
+driver.get(urls[0])
+
+def scrape_match_links(driver, url, sample_size = 250):
+
+	driver.get(url)
+
+	#Load more matches...
+	n_sampled = 0
+	n_ticks = 0
+
+	while n_sampled < sample_size and n_ticks < 7:
+
+		try:
+
+			n_ticks = 0
+
+			buttons = driver.find_elements_by_class_name("trn-button")
+			load_more_button = load_more_button = [b for b in buttons if b.text in ["Load More Matches", "View All Matches"]]
+
+			if len(load_more_button) == 0:
+				n_ticks += 1
+				continue
+
+			ActionChains(driver).click(load_more_button[0]).perform()
+
+			n_sampled = len(driver.find_elements_by_class_name("match-row__link"))
+
+		except:
+
+			continue
+
+
+	match_links = [x.get_attribute("href") for x in driver.find_elements_by_class_name("match-row__link")]
+
+	return match_links
+
+
+
+def extract_stats(driver, url):
+
+	driver.get(url)
+	stats = driver.find_elements_by_class_name("stats")
+	while len(stats) == 0:
+		stats = driver.find_elements_by_class_name("stats")
+
+	stats = [x.text for x in stats]
+
+	game_type = driver.find_element_by_class_name("title").text
+	if game_type == "BR Solos":
+
+		stats = [x for x in stats if not "K/D" in x and not "Kills" in x]
+		stats = [x for x in stats if "#" in x]
+		kdrs = [float(x.split("\n")[0]) for x in stats]
+
+	else:
+
+		stats = [x for x in stats if "K/D" in x]
+		kdrs = [float(x.split("\n")[1]) for x in stats]
+
+	if "Quads" in game_type:
+		del kdrs[0:4]
+	elif "Trios" in game_type:
+		del kdrs[0:3]
+	elif "Duos" in game_type:
+		del kdrs[0:2]
+	else:
+		del kdrs[0]
+
+	game_info = driver.find_element_by_class_name("info").text
+
+	game_date = game_info.split(",")[0]
+	game_time = game_info.split(",")[-1].split("\n")[0].strip()
+
+	output = pd.DataFrame(kdrs)
+	output.columns = ["KDR"]
+	output["Date"] = game_date
+	output["Time"] = game_time
+	output["Mode"] = game_type
+
+	return output
+
+
+
+def extract_stats_vectorized(driver, urls):
+
+	kdrs = []
+
+	n = len(urls)
+	counter = 0
+	for x in tqdm(urls):
+
+		counter += 1
+
+		try:
+
+			kdrs.append(extract_stats(driver, x))
+
+		except:
+
+			continue
+
+		if counter % 10 == 0:
+
+			sample_size = np.sum([len(x) for x in kdrs])
+			print("Sample size:" + str(sample_size))
+
+	return pd.concat(kdrs, 0).reset_index(drop = True)
+
+
+
+#Get matches urls
+matches = [scrape_match_links(driver, url) for url in urls]
+kdrs = [extract_stats_vectorized(driver, match_urls) for match_urls in matches]
+
+names = ["FrankFredj%231458", "Huskerrs", "SwaggXBL"]
+for frame, name in zip(kdrs, names):
+	frame.to_csv("D:/CoD_data/" + name + ".csv")
